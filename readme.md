a7c48da333da781622883417279a61334809abfe
kastoq <74154121+kastoq@users.noreply.github.com>
2021-01-13 18:03:35 +0100

    Add files via upload

--- readme.md
+++ readme.md
@@ -7,9 +7,15 @@ Nb. de standaardstructuur en de spartaanse opmaak zijn helemaal prima. Het gaat 
 
 
 ## Bronnenlijst
-1. -bron 1-
-2. -bron 2-
-3. -...-
+1. https://flexbox.help/
+2. w3schools.com
+3. stackoverflow.com
+4. https://developer.mozilla.org
+5. https://css-tricks.com/
+6. gegeven theorie van het vak
+7. opdrachten van het vak
+8. gekregen tips van de voortgangsgesprekken
+
 
 
 
@@ -17,37 +23,45 @@ Nb. de standaardstructuur en de spartaanse opmaak zijn helemaal prima. Het gaat 
 
 -dit ging goed & dit was lastig-
 
+ik weet niet zeker of mijn microinteractie voldoende is.
+nogal wat twijfels over of de gehele verzorging zowel visueel op de website (en dan ook op alle niveaus van responsiveness als de code zelf wel helemaal goed is.
+
 **Screenshot(s):**
-
--screenshot(s) van je eindresultaat-
-
+<img src="images/Pokemon site - screenshot dekstop.png" width="375px" alt="homepage desktop">
+<img src="images/Pokemon site - screenshot mobile.png" width="375px" alt="homepage mobiel">
+<img src="images/Pokemon site detailpagina - screenshot dekstop.png" width="375px" alt="detailpagina desktop">
+<img src="images/Pokemon site detailpagina - screenshot mobile.png" width="375px" alt="detailpagina mobiel">
 
 
 ## Voortgang 3 (week 6)
 
-
--dit ging goed & dit was lastig-
+Niet veel verandering/voortgang in vergelijking tot gesprek 2.
+Gehele voortgang is getoond aan de studentassistent (hij had de website nog alleen t/m voortgang 1 gezien).
+Daarom gevraagd om tips van wat er beter/toegevoegd kon worden.
+tip gekregen om ook een "scroll-to-top"button toe te voegen als extra javascript onderdeel.
 
 
 
 ## Voortgang 2 (week 5)
 
+vragen die ik had heb ik hier gesteld gesteld.
 goede hoeveelheid content.
 slider hoeft niet helemaal mooi werkend te zijn (teveel onnodige moeite).
 Microinteractie mist nog (denk na over wat dat voor mijn website zou kunnen worden).
-
+handige tip over positionering in het midden gekregen.
 
 
 
 ## Voortgang 1 (week 3)
 
+vragen die ik had heb ik hier gesteld.
 goede hoeveelheid content.
 zitten hier en daar nog wat onderdelen die "fixing" nodig hebben
 classes moeten nog weggewerkt worden
 sommige onderdelen kunnen beter een flexbox of grid gebruiken i.p.v. te spelen met posities.
+tips over hoe de carrousel kan laten werken gekregen.
 
 -dit ging goed & dit was lastig-
-
 onderdelen op de juiste plek (en mooi) krijgen
 het carrousel
 vormgeving (kleur en vorm van sommige onderdelen)
@@ -87,7 +101,7 @@ javascript is dus nog niet helemaal duidelijk voor me.
 ik ben ook niet zeker over mijn niveau voor HTML & Css 
 
 **Je focus:** -kies uit responsive óf surface plane- 
-surface, voor nu. misschien denk ik daar later anders over.
+responsive
 
 **Je opdracht:** -link naar de website die je gaat namaken óf de naam van je eigen ontwerp- 
 pokemon.com/nl
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,139 @@
 // JavaScript Document
+
+/* scrolltop button*/
+scrolltop = document.querySelector("div.scrolltop");
+scrolltop.addEventListener("click", rise);
+
+function rise() {
+  window.scrollTo(0, 0);
+}
+
+///* pokémon picker *///
+
+/*froakie*/
+var b1 = document.querySelector("li.deximage1");
+
+b1.addEventListener("click", change1);
+
+function change1() {
+  p1 = document.querySelector("div.pokemon img:first-of-type");
+  p2 = document.querySelector("div.pokemon img:nth-of-type(2)");
+  p3 = document.querySelector("div.pokemon img:nth-of-type(3)");
+  p4 = document.querySelector("div.pokemon img:nth-of-type(4)");
+  p5 = document.querySelector("div.pokemon img:nth-of-type(5)");
+  p6 = document.querySelector("div.pokemon img:nth-of-type(6)");
+
+  p1.classList.add("anders");
+  p2.classList.remove("anders");
+  p3.classList.remove("anders");
+  p4.classList.remove("anders");
+  p5.classList.remove("anders");
+  p6.classList.remove("anders");
+}
+
+
+
+/*fennekin*/
+var b2 = document.querySelector("li.deximage2");
+
+b2.addEventListener("click", change2);
+
+function change2() {
+  p1 = document.querySelector("div.pokemon img:first-of-type");
+  p2 = document.querySelector("div.pokemon img:nth-of-type(2)");
+  p3 = document.querySelector("div.pokemon img:nth-of-type(3)");
+  p4 = document.querySelector("div.pokemon img:nth-of-type(4)");
+  p5 = document.querySelector("div.pokemon img:nth-of-type(5)");
+  p6 = document.querySelector("div.pokemon img:nth-of-type(6)");
+
+  p1.classList.remove("anders");
+  p2.classList.add("anders");
+  p3.classList.remove("anders");
+  p4.classList.remove("anders");
+  p5.classList.remove("anders");
+  p6.classList.remove("anders");
+}
+
+/*chespin*/
+var b3 = document.querySelector("li.deximage3");
+
+b3.addEventListener("click", change3);
+
+function change3() {
+  p1 = document.querySelector("div.pokemon img:first-of-type");
+  p2 = document.querySelector("div.pokemon img:nth-of-type(2)");
+  p3 = document.querySelector("div.pokemon img:nth-of-type(3)");
+  p4 = document.querySelector("div.pokemon img:nth-of-type(4)");
+  p5 = document.querySelector("div.pokemon img:nth-of-type(5)");
+  p6 = document.querySelector("div.pokemon img:nth-of-type(6)");
+
+  p1.classList.remove("anders");
+  p2.classList.remove("anders");
+  p3.classList.add("anders");
+  p4.classList.remove("anders");
+  p5.classList.remove("anders");
+  p6.classList.remove("anders");
+}
+
+/*mudkip*/
+var b4 = document.querySelector("li.deximage4");
+
+b4.addEventListener("click", change4);
+
+function change4() {
+  p1 = document.querySelector("div.pokemon img:first-of-type");
+  p2 = document.querySelector("div.pokemon img:nth-of-type(2)");
+  p3 = document.querySelector("div.pokemon img:nth-of-type(3)");
+  p4 = document.querySelector("div.pokemon img:nth-of-type(4)");
+  p5 = document.querySelector("div.pokemon img:nth-of-type(5)");
+  p6 = document.querySelector("div.pokemon img:nth-of-type(6)");
+
+  p1.classList.remove("anders");
+  p2.classList.remove("anders");
+  p3.classList.remove("anders");
+  p4.classList.add("anders");
+  p5.classList.remove("anders");
+  p6.classList.remove("anders");
+}
+
+/*torchic*/
+var b5 = document.querySelector("li.deximage5");
+
+b5.addEventListener("click", change5);
+
+function change5() {
+  p1 = document.querySelector("div.pokemon img:first-of-type");
+  p2 = document.querySelector("div.pokemon img:nth-of-type(2)");
+  p3 = document.querySelector("div.pokemon img:nth-of-type(3)");
+  p4 = document.querySelector("div.pokemon img:nth-of-type(4)");
+  p5 = document.querySelector("div.pokemon img:nth-of-type(5)");
+  p6 = document.querySelector("div.pokemon img:nth-of-type(6)");
+
+  p1.classList.remove("anders");
+  p2.classList.remove("anders");
+  p3.classList.remove("anders");
+  p4.classList.remove("anders");
+  p5.classList.add("anders");
+  p6.classList.remove("anders");
+}
+
+/*treecko*/
+var b6 = document.querySelector("li.deximage6");
+
+b6.addEventListener("click", change6);
+
+function change6() {
+  p1 = document.querySelector("div.pokemon img:first-of-type");
+  p2 = document.querySelector("div.pokemon img:nth-of-type(2)");
+  p3 = document.querySelector("div.pokemon img:nth-of-type(3)");
+  p4 = document.querySelector("div.pokemon img:nth-of-type(4)");
+  p5 = document.querySelector("div.pokemon img:nth-of-type(5)");
+  p6 = document.querySelector("div.pokemon img:nth-of-type(6)");
+
+  p1.classList.remove("anders");
+  p2.classList.remove("anders");
+  p3.classList.remove("anders");
+  p4.classList.remove("anders");
+  p5.classList.remove("anders");
+  p6.classList.add("anders");
+}
